f3f8baac360738d56dd38a432cb5e19ad3f7762d
Jacob Prud'homme <2160185+jacobprudhomme@users.noreply.github.com>
2026-02-19 11:20:48 +0100

    Remove mutability requirement on self for non-mutating exponentiation

--- src/traits/fp.rs
+++ src/traits/fp.rs
@@ -154,11 +154,11 @@ pub trait Fp:
     /// MUST be at most `ebitlen`. This is constant-time for both the
     /// base value (`self`) and the exponent (`e`); the exponent maximum
     /// size (`ebitlen`) is considered non-secret.
-    fn pow_u64(self, e: u64, ebitlen: usize) -> Self;
+    fn pow_u64(&self, e: u64, ebitlen: usize) -> Self;
 
     /// Return this value to the power e. The exponent is considered
     /// non-secret.
-    fn pow_u64_vartime(self, e: u64) -> Self;
+    fn pow_u64_vartime(&self, e: u64) -> Self;
 
     // Return this value to the provided exponent. The exponent is non-zero
     // and is public. The exponent is encoded over N 64-bit limbs.
